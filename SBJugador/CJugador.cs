40120fd55bf53ad92ae45ba7371ae97595779d53
Monarca117 <skyggemonarken@gmail.com>
2024-04-09 12:50:17 -0600

    Version 1.0.0.0

---
+++ SBJugador/CJugador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBJugador
+{
+    [Serializable]
+    internal class CJugador
+    {
+        private String name;
+        private String role;
+        private int level;
+
+        public CJugador(String pName, string pRole, int pLevel)
+        {
+            name = pName;
+            role = pRole;
+            level = pLevel;
+        }
+
+        public void MuestraInformacion()
+        {
+            //Mostramos la información necesaria
+            Console.WriteLine("Nombre de jugador: {0}", name);
+            Console.WriteLine("El rol del jugador es: {0}", role);
+            Console.WriteLine("El nivel del jugador es: {0}", level);
+            Console.WriteLine("------------------------------");
+        }
+    }
+}
---
+++ SBJugador/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization.Formatters.Binary;  //El formateador binario toma la información de la memoria bit pot bi y lo guarda.
+using System.IO; //Es usado para tener los mecanismos de entrada y salida
+
+namespace SBJugador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+            String valor = "";
+
+            Console.WriteLine("1.- Crear y serializar jugador, 2.- Leer jugador");
+            valor = Console.ReadLine();
+            opcion = Convert.ToInt32(valor);
+
+            if (opcion == 1)
+            {
+                //Creamos el objeto auto
+                string name = "";
+                string role = "";
+                int level = 0;
+
+                Console.WriteLine("Escribe el nombre del jugador");
+                name = Console.ReadLine();
+
+                Console.WriteLine("Escribe tu rol ");
+                role = Console.ReadLine();
+
+                Console.WriteLine("Escribe el nivel del jugador");
+                valor = Console.ReadLine();
+                level = Convert.ToInt32(valor);
+
+                CJugador miJugador = new CJugador(name, role, level);
+
+                Console.WriteLine("Jugador a serializar");
+                miJugador.MuestraInformacion();
+
+                //Empezamos la serialización
+                Console.WriteLine("---SERIALIZANDO---");
+
+                //
+                BinaryFormatter formateador = new BinaryFormatter();
+
+                //Creamos el stream
+                Stream miStream = new FileStream("Jugador.jug", FileMode.Create, FileAccess.Write, FileShare.None);
+
+                //Serializamos
+                formateador.Serialize(miStream, miJugador);
+
+                //Cerramos el stream
+                miStream.Close();
+            }
+
+            if (opcion == 2)
+            {
+                //Deserializamos el objeto
+                Console.WriteLine("---DESERIALIZAMOS---");
+
+                //Seleccionamos el formateador
+                BinaryFormatter formateador = new BinaryFormatter();
+
+                //Creamos el stream
+                Stream miStream = new FileStream("Jugador.jug", FileMode.Open, FileAccess.Read, FileShare.None);
+
+                //Deserializamos
+                CJugador miJugador = (CJugador)formateador.Deserialize(miStream);
+
+                //Cerramos el stream
+                miStream.Close();
+
+                //Usamos el objeto
+                Console.WriteLine("El jugador deserializado es ");
+                miJugador.MuestraInformacion();
+
+            }
+        }
+    }
+}
